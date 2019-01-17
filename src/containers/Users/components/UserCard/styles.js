3815e09b9608c379b5e2782f4f2fb18a50779782
Spencer Tiberi <spencertiberi@gmail.com>
2019-01-17 17:00:31 -0500

    Add UserCard

---
+++ src/containers/Users/components/UserCard/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+import { Flex } from 'grid-styled'
+
+export const Container = styled(Flex)`
+  flex-direction: column;
+  height: 150px;
+  width: 150px;
+  margin: 5px;
+  align-items: center;
+  justify-content: center;
+  background-color: purple;
+`
+
+export const Name = styled(Flex)`
+  width: 150px;
+  height: 50px;
+  text-align: center;
+  color: white;
+  font-size: 1rem;
+`
+
+export const Image = styled.img`
+  width: 150px;
+  height: 100px;
+`
