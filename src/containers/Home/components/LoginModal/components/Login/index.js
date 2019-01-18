6ca799b61ea6536ed2763374fa1274a277ae1697
docovarr2020 <diego.o.co@gmail.com>
2019-01-17 21:54:58 -0500

    Add state management to Signup/Login forms

--- src/containers/Home/components/LoginModal/components/Login/index.js
+++ src/containers/Home/components/LoginModal/components/Login/index.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react'
 import { Title, LineInput, SubmitButton, SecondaryOptionText } from './styles'
 
 class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  onChange = (key, e) => {
+    this.setState({ [key]: e.target.value })
+  }
+
   render() {
     return (
       <React.Fragment>
         <Title>Welcome Back!</Title>
-        <LineInput placeholder="Email" />
-        <LineInput placeholder="Password" />
+        <LineInput
+          placeholder="Email"
+          onChange={e => this.onChange('email', e)}
+        />
+        <LineInput
+          placeholder="Password"
+          onChange={e => this.onChange('password', e)}
+          type="password"
+        />
         <SubmitButton>Login</SubmitButton>
         <SecondaryOptionText onClick={this.props.changeMode}>
           Or Sign Up
--- src/containers/Home/components/LoginModal/components/SignUp/index.js
+++ src/containers/Home/components/LoginModal/components/SignUp/index.js
@@ -2,12 +2,31 @@ import React, { Component } from 'react'
 import { Title, LineInput, SubmitButton, SecondaryOptionText } from './styles'
 
 class SignUp extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  onChange = (key, e) => {
+    this.setState({ [key]: e.target.value })
+  }
+
   render() {
     return (
       <React.Fragment>
         <Title>Nice to meet you!</Title>
-        <LineInput placeholder="Email" />
-        <LineInput placeholder="Password" />
+        <LineInput
+          placeholder="Email"
+          onChange={e => this.onChange('email', e)}
+        />
+        <LineInput
+          placeholder="Password"
+          onChange={e => this.onChange('password', e)}
+          type="password"
+        />
         <SubmitButton>Get Started</SubmitButton>
         <SecondaryOptionText onClick={this.props.changeMode}>
           Or Login
